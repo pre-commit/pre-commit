47b7ca44ed1fcaa83464ed00cef72049ae22c33d
Anthony Sottile <asottile@umich.edu>
2014-03-22 17:21:17 -0700

    Improve failure message for pre-commit.  Closes #36

--- pre_commit/resources/pre-commit.sh
+++ pre_commit/resources/pre-commit.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
+which pre-commit > /dev/null
+if [ $? -ne 0 ]; then
+    echo '`pre-commit` not found.  Did you forget to activate your virtualenv?'
+    exit 1
+fi
+
 pre-commit
