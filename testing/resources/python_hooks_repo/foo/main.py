3d5a360df45431b71bbb2fdd9ebd183b060a13ec
Anthony Sottile <asottile@umich.edu>
2014-04-13 18:07:21 -0700

    Fix Py3

--- testing/resources/python_hooks_repo/foo/main.py
+++ testing/resources/python_hooks_repo/foo/main.py
@@ -1,7 +1,8 @@
+from __future__ import print_function
 import sys
 
 
 def func():
-    print repr(sys.argv[1:])
-    print 'Hello World'
+    print(repr(sys.argv[1:]))
+    print('Hello World')
     return 0
