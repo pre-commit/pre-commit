619bca2bdcade2f5a38ce214617964b7ba360152
Anthony Sottile <asottile@umich.edu>
2014-06-01 16:06:50 -0700

    Use Python3.4 on travis.

--- setup.py
+++ setup.py
@@ -17,6 +17,7 @@ setup(
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: Implementation :: PyPy',
     ],
 
