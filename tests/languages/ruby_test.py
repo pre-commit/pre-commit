bee56cd5bc619540a93f05aa60033a1d2009f8ed
Anthony Sottile <asottile@umich.edu>
2014-06-23 13:50:40 -0700

    Use our archives instead of pulling from gits.

--- tests/languages/ruby_test.py
+++ tests/languages/ruby_test.py
@@ -3,16 +3,12 @@ from __future__ import unicode_literals
 import os.path
 
 from pre_commit.languages.ruby import _install_rbenv
-from testing.util import skipif_slowtests_false
 
 
-@skipif_slowtests_false
 def test_install_rbenv(cmd_runner):
     _install_rbenv(cmd_runner)
     # Should have created rbenv directory
     assert os.path.exists(cmd_runner.path('rbenv'))
-    # It should be a git checkout
-    assert os.path.exists(cmd_runner.path('rbenv', '.git'))
     # We should have created our `activate` script
     activate_path = cmd_runner.path('rbenv', 'bin', 'activate')
     assert os.path.exists(activate_path)
@@ -27,7 +23,6 @@ def test_install_rbenv(cmd_runner):
     )
 
 
-@skipif_slowtests_false
 def test_install_rbenv_with_version(cmd_runner):
     _install_rbenv(cmd_runner, version='1.9.3p547')
 
