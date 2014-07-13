720db97c1328b180f611f3f5495c170b4779b65b
Anthony Sottile <asottile@umich.edu>
2014-07-13 16:58:00 -0700

    More accurate classifiers.

--- setup.py
+++ setup.py
@@ -17,10 +17,13 @@ setup(
     platforms='linux',
     classifiers=[
         'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
     ],
 
