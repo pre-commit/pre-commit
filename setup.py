dbae23538f44bace7b53b1ce51e5e101467a7144
Anthony Sottile <asottile@umich.edu>
2014-07-17 16:56:41 -0700

    v0.2.5

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
         'hooks.'
     ),
     url='https://github.com/pre-commit/pre-commit',
-    version='0.2.4',
+    version='0.2.5',
 
     author='Anthony Sottile',
     author_email='asottile@umich.edu',
