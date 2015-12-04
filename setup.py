2df1dc90232ad8e9985718c14025bd6e6738a52b
Anthony Sottile <asottile@umich.edu>
2015-12-04 13:35:24 -0800

    Add python3.5, pypy3, and latest git to travis

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
     ],
