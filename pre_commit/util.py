a4884245013bb723d7594a6140a59f4754f36b44
Anthony Sottile <asottile@umich.edu>
2015-12-22 13:50:21 -0800

    Use python3.5 in appveyor over py34

--- pre_commit/util.py
+++ pre_commit/util.py
@@ -175,7 +175,10 @@ def rmtree(path):
     """On windows, rmtree fails for readonly dirs."""
     def handle_remove_readonly(func, path, exc):  # pragma: no cover (windows)
         excvalue = exc[1]
-        if func in (os.rmdir, os.remove) and excvalue.errno == errno.EACCES:
+        if (
+                func in (os.rmdir, os.remove, os.unlink) and
+                excvalue.errno == errno.EACCES
+        ):
             os.chmod(path, stat.S_IRWXU | stat.S_IRWXG | stat.S_IRWXO)
             func(path)
         else:
