fa06e72f0172b6888e0f3430c317885290956e86
Anthony Sottile <asottile@umich.edu>
2017-03-30 08:43:50 -0700

    Add a `pre-commit sample-config` command

---
+++ pre_commit/commands/sample_config.py
@@ -0,0 +1,25 @@
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import unicode_literals
+
+
+# TODO: maybe `git ls-remote git://github.com/pre-commit/pre-commit-hooks` to
+# determine the latest revision?  This adds ~200ms from my tests (and is
+# significantly faster than https:// or http://).  For now, periodically
+# manually updating the revision is fine.
+SAMPLE_CONFIG = '''\
+# See http://pre-commit.com for more information
+# See http://pre-commit.com/hooks.html for more hooks
+-   repo: https://github.com/pre-commit/pre-commit-hooks
+    sha: v0.7.1
+    hooks:
+    -   id: trailing-whitespace
+    -   id: end-of-file-fixer
+    -   id: check-yaml
+    -   id: check-added-large-files
+'''
+
+
+def sample_config():
+    print(SAMPLE_CONFIG, end='')
+    return 0
---
+++ tests/commands/sample_config_test.py
@@ -0,0 +1,21 @@
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+from pre_commit.commands.sample_config import sample_config
+
+
+def test_sample_config(capsys):
+    ret = sample_config()
+    assert ret == 0
+    out, _ = capsys.readouterr()
+    assert out == '''\
+# See http://pre-commit.com for more information
+# See http://pre-commit.com/hooks.html for more hooks
+-   repo: https://github.com/pre-commit/pre-commit-hooks
+    sha: v0.7.1
+    hooks:
+    -   id: trailing-whitespace
+    -   id: end-of-file-fixer
+    -   id: check-yaml
+    -   id: check-added-large-files
+'''
