98a6fce830f6dc0bb201b42b6ffc40da42376f9f
Theresa Ma <tma@yelp.com>
2018-02-24 11:19:13 -0800

    update swift to swift 4

--- get-swift.sh
+++ get-swift.sh
@@ -4,9 +4,9 @@ set -ex
 
 . /etc/lsb-release
 if [ "$DISTRIB_CODENAME" = "trusty" ]; then
-    SWIFT_URL='https://swift.org/builds/swift-3.1.1-release/ubuntu1404/swift-3.1.1-RELEASE/swift-3.1.1-RELEASE-ubuntu14.04.tar.gz'
+    SWIFT_URL='https://swift.org/builds/swift-4.0.3-release/ubuntu1404/swift-4.0.3-RELEASE/swift-4.0.3-RELEASE-ubuntu14.04.tar.gz'
 else
-    SWIFT_URL='https://swift.org/builds/swift-3.1.1-release/ubuntu1604/swift-3.1.1-RELEASE/swift-3.1.1-RELEASE-ubuntu16.04.tar.gz'
+    SWIFT_URL='https://swift.org/builds/swift-4.0.3-release/ubuntu1604/swift-4.0.3-RELEASE/swift-4.0.3-RELEASE-ubuntu16.04.tar.gz'
 fi
 
 mkdir -p /tmp/swift
