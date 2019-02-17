136834038d915d46fb93ea88ec76251158732686
Anthony Sottile <asottile@umich.edu>
2019-02-17 10:13:49 -0800

    Use npm install git+file:// instead of npm install .

--- pre_commit/languages/node.py
+++ pre_commit/languages/node.py
@@ -62,10 +62,11 @@ def install_environment(prefix, version, additional_dependencies):
             cmd.extend(['-n', version])
         cmd_output(*cmd)
 
+        dep = 'git+file://{}'.format(prefix.prefix_dir)
         with in_env(prefix, version):
             helpers.run_setup_cmd(
                 prefix,
-                ('npm', 'install', '-g', '.') + additional_dependencies,
+                ('npm', 'install', '-g', dep) + additional_dependencies,
             )
 
 
