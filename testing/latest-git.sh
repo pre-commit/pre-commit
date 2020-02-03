8d2af32e4d02de4a2e3c70bccd337fd738a47a56
Anthony Sottile <asottile@umich.edu>
2020-02-03 14:06:51 -0800

    delete unused testing/latest-git.sh

--- testing/latest-git.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# This is a script used in travis-ci to have latest git
-set -ex
-git clone git://github.com/git/git --depth 1 /tmp/git
-pushd /tmp/git
-make prefix=/tmp/git -j8 install
-popd
