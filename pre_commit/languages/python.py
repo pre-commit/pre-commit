0781dac78f87729fbbe0eb830561da57d2183f58
Anthony Sottile <asottile@umich.edu>
2020-05-27 13:14:29 -0700

    avoid a UnicodeError on windows with non-charmap characters

--- pre_commit/languages/python.py
+++ pre_commit/languages/python.py
@@ -67,9 +67,10 @@ def _find_by_py_launcher(
 ) -> Optional[str]:  # pragma: no cover (windows only)
     if version.startswith('python'):
         num = version[len('python'):]
+        cmd = ('py', f'-{num}', '-c', 'import sys; print(sys.executable)')
+        env = dict(os.environ, PYTHONIOENCODING='UTF-8')
         try:
-            cmd = ('py', f'-{num}', '-c', 'import sys; print(sys.executable)')
-            return cmd_output(*cmd)[1].strip()
+            return cmd_output(*cmd, env=env)[1].strip()
         except CalledProcessError:
             pass
     return None
