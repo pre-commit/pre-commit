91d9e5f79e25eb004de648be9a0de2b6960feea7
Jason Hollis <jhollis@jdubb.net>
2020-12-06 12:17:51 -0500

    Fixed sample-config to proper yaml formatting
        * Fixed yaml formatting
        * bumped version in sample config :)

--- pre_commit/commands/sample_config.py
+++ pre_commit/commands/sample_config.py
@@ -3,16 +3,18 @@
 # significantly faster than https:// or http://).  For now, periodically
 # manually updating the revision is fine.
 SAMPLE_CONFIG = '''\
+---
+
 # See https://pre-commit.com for more information
 # See https://pre-commit.com/hooks.html for more hooks
 repos:
--   repo: https://github.com/pre-commit/pre-commit-hooks
-    rev: v3.2.0
+  - repo: https://github.com/pre-commit/pre-commit-hooks
+    rev: v3.3.0
     hooks:
-    -   id: trailing-whitespace
-    -   id: end-of-file-fixer
-    -   id: check-yaml
-    -   id: check-added-large-files
+      - id: trailing-whitespace
+      - id: end-of-file-fixer
+      - id: check-yaml
+      - id: check-added-large-files
 '''
 
 
