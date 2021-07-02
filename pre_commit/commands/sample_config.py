4ae57bab8ce0581f97ad727bea381ff4880b4e93
Schäfer, Denny <denny.schaefer@immowelt.de>
2021-07-02 15:15:05 +0200

    Update sample config pre-commit-hooks version to v4.0.1

--- pre_commit/commands/sample_config.py
+++ pre_commit/commands/sample_config.py
@@ -7,7 +7,7 @@ SAMPLE_CONFIG = '''\
 # See https://pre-commit.com/hooks.html for more hooks
 repos:
 -   repo: https://github.com/pre-commit/pre-commit-hooks
-    rev: v3.2.0
+    rev: v4.0.1
     hooks:
     -   id: trailing-whitespace
     -   id: end-of-file-fixer
--- tests/commands/sample_config_test.py
+++ tests/commands/sample_config_test.py
@@ -10,7 +10,7 @@ def test_sample_config(capsys):
 # See https://pre-commit.com/hooks.html for more hooks
 repos:
 -   repo: https://github.com/pre-commit/pre-commit-hooks
-    rev: v3.2.0
+    rev: v4.0.1
     hooks:
     -   id: trailing-whitespace
     -   id: end-of-file-fixer
