c8cf74dc718ee42cb6db8c0ac7dccc25b219474d
Anthony Sottile <asottile@umich.edu>
2021-10-23 13:23:48 -0400

    replace exit(main()) with raise SystemExit(main())
    Committed via https://github.com/asottile/all-repos

--- pre_commit/main.py
+++ pre_commit/main.py
@@ -411,4 +411,4 @@ def main(argv: Optional[Sequence[str]] = None) -> int:
 
 
 if __name__ == '__main__':
-    exit(main())
+    raise SystemExit(main())
--- pre_commit/meta_hooks/check_hooks_apply.py
+++ pre_commit/meta_hooks/check_hooks_apply.py
@@ -39,4 +39,4 @@ def main(argv: Optional[Sequence[str]] = None) -> int:
 
 
 if __name__ == '__main__':
-    exit(main())
+    raise SystemExit(main())
