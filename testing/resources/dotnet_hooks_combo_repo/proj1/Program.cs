de442cafb394dd330aeff00e3924ea5ab511d342
Walluce Pinkham <Walluce.Pinkham@derivco.co.uk>
2022-04-07 15:14:18 +0100

    Added tests for new code

---
+++ testing/resources/dotnet_hooks_combo_repo/proj1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace proj1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello from dotnet!\n");
+        }
+    }
+}
