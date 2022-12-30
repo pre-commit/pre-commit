cddaa0dddc7e1fab506795287007aff50e88b592
Anthony Sottile <asottile@umich.edu>
2022-12-29 22:56:58 -0500

    r is installed by default on GHA

--- testing/get-r.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-sudo apt install r-base
-# create empty folder for user library.
-# necessary for non-root users who have
-# never installed an R package before.
-# Alternatively, we require the renv
-# package to be installed already, then we can
-# omit that.
-Rscript -e 'dir.create(Sys.getenv("R_LIBS_USER"), recursive = TRUE)'
