887c5e1142ea9022407e85e7319bec3a403e1572
Anthony Sottile <asottile@umich.edu>
2022-12-29 20:54:03 -0500

    azure pipelines -> github actions

--- testing/get-dart.sh
+++ testing/get-dart.sh
@@ -5,10 +5,10 @@ VERSION=2.13.4
 
 if [ "$OSTYPE" = msys ]; then
     URL="https://storage.googleapis.com/dart-archive/channels/stable/release/${VERSION}/sdk/dartsdk-windows-x64-release.zip"
-    echo "##vso[task.prependpath]$(cygpath -w /tmp/dart-sdk/bin)"
+    cygpath -w /tmp/dart-sdk/bin >> "$GITHUB_PATH"
 else
     URL="https://storage.googleapis.com/dart-archive/channels/stable/release/${VERSION}/sdk/dartsdk-linux-x64-release.zip"
-    echo '##vso[task.prependpath]/tmp/dart-sdk/bin'
+    echo '/tmp/dart-sdk/bin' >> "$GITHUB_PATH"
 fi
 
 curl --silent --location --output /tmp/dart.zip "$URL"
