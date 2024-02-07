e58009684cfc4842028e99d34837e2722af39b93
Gaëtan Lehmann <gaetan.lehmann@gmail.com>
2024-02-07 11:18:24 +0100

    give docker a tty output when expecting color
    this makes the behavior more consistent with the system language
    and would help the executable run in a docker container to
    produce a colored output.

--- tests/languages/docker_image_test.py
+++ tests/languages/docker_image_test.py
@@ -25,3 +25,27 @@ def test_docker_image_hook_via_args(tmp_path):
         args=('hello hello world',),
     )
     assert ret == (0, b'hello hello world\n')
+
+
+@xfailif_windows  # pragma: win32 no cover
+def test_docker_image_color_tty(tmp_path):
+    ret = run_language(
+        tmp_path,
+        docker_image,
+        'ubuntu:22.04',
+        args=('grep', '--color', 'root', '/etc/group'),
+        color=True,
+    )
+    assert ret == (0, b'\x1b[01;31m\x1b[Kroot\x1b[m\x1b[K:x:0:\n')
+
+
+@xfailif_windows  # pragma: win32 no cover
+def test_docker_image_no_color_no_tty(tmp_path):
+    ret = run_language(
+        tmp_path,
+        docker_image,
+        'ubuntu:22.04',
+        args=('grep', '--color', 'root', '/etc/group'),
+        color=False,
+    )
+    assert ret == (0, b'root:x:0:\n')
